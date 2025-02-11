2eb57c3367df595555c3ede1af8de6ab45f551e8
apokryff <megagugl530@gmail.com>
2025-02-11 20:51:41 +0300

    Add shell wrapper for yazi

--- .config/zsh/config/functions.zsh
+++ .config/zsh/config/functions.zsh
@@ -9,3 +9,12 @@ clear_docker() {
   docker rm $(docker ps -aq)
   docker rmi $(docker images -q)
 }
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
